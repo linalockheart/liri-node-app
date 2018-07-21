cda4618cd74a331ffefb37d08eb521ca1c09d198
Lina Lockheart <angelinalockheart@gmail.com>
2018-07-21 14:48:04 -0400

    cleaned up OMDB function

--- liri.js
+++ liri.js
@@ -174,25 +174,8 @@ function getMovie() {
     }
 
     else {
-
         movieName = "Mr. Nobody";
-        var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-
-        request(queryUrl, function(error, response, body) {
-
-            if (!error && response.statusCode === 200) {
-                console.log("Movie Title: " + JSON.parse(body).Title);
-                console.log("Release Year: " + JSON.parse(body).Year);
-                console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
-                console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-                console.log("Country: " + JSON.parse(body).Country);
-                console.log("Language: " + JSON.parse(body).Language);
-                console.log("Plot: " + JSON.parse(body).Plot);
-                console.log("Actors: " + JSON.parse(body).Actors);
-                console.log("==============================================");
-            }
-        });
-
+        getMovie();
     }
 }
 
